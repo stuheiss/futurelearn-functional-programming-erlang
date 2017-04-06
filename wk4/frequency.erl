32cd9191f82c08ba52e12e60b464a2f49f2b0c7e
Stu Heiss <sheiss@highlandsolutions.com>
2017-04-06 08:51:48 -0500

    Add asset frequency.erl

---
+++ wk4/frequency.erl
@@ -0,0 +1,48 @@
+%% Based on code from 
+%%   Erlang Programming
+%%   Francecso Cesarini and Simon Thompson
+%%   O'Reilly, 2008
+%%   http://oreilly.com/catalog/9780596518189/
+%%   http://www.erlangprogramming.org/
+%%   (c) Francesco Cesarini and Simon Thompson
+
+-module(frequency).
+-export([init/0]).
+
+%% These are the start functions used to create and
+%% initialize the server.
+
+init() ->
+  Frequencies = {get_frequencies(), []},
+  loop(Frequencies).
+
+% Hard Coded
+get_frequencies() -> [10,11,12,13,14,15].
+
+%% The Main Loop
+
+loop(Frequencies) ->
+  receive
+    {request, Pid, allocate} ->
+      {NewFrequencies, Reply} = allocate(Frequencies, Pid),
+      Pid ! {reply, Reply},
+      loop(NewFrequencies);
+    {request, Pid , {deallocate, Freq}} ->
+      NewFrequencies = deallocate(Frequencies, Freq),
+      Pid ! {reply, ok},
+      loop(NewFrequencies);
+    {request, Pid, stop} ->
+      Pid ! {reply, stopped}
+  end.
+
+%% The Internal Help Functions used to allocate and
+%% deallocate frequencies.
+
+allocate({[], Allocated}, _Pid) ->
+  {{[], Allocated}, {error, no_frequency}};
+allocate({[Freq|Free], Allocated}, Pid) ->
+  {{Free, [{Freq, Pid}|Allocated]}, {ok, Freq}}.
+
+deallocate({Free, Allocated}, Freq) ->
+  NewAllocated=lists:keydelete(Freq, 1, Allocated),
+  {[Freq|Free],  NewAllocated}.
