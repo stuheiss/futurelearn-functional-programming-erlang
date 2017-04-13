56735057f10f4fd933d004ed28d6aafdadb8a98b
Stu Heiss <sheiss@highlandsolutions.com>
2017-04-13 08:48:03 -0500

    supervised frequency server

--- wk5/frequency.erl
+++ wk5/frequency.erl
@@ -9,16 +9,83 @@
 -module(frequency).
 -export([start/0,allocate/0,deallocate/1,stop/0]).
 -export([init/0]).
+-export([start_sup/0,stop_sup/0,kill_sup/0,kill_freq/0,kill_all/0]).
+-export([spawn_client/1,client/1]).
+-export([w/0,dump/0]).
+
+% spawn a client model process
+spawn_client(Sleep) ->
+  spawn(fun()->client(Sleep) end).
+
+% simple client model
+client(Sleep) ->
+  {ok,Freq} = allocate(),
+  io:format("~p client start call on ~w for ~wms~n",[self(),Freq,Sleep]),
+  timer:sleep(Sleep),
+  io:format("~p client end call on ~w after ~wms~n",[self(),Freq,Sleep]),
+  deallocate(Freq).
 
 %% These are the start functions used to create and
 %% initialize the server.
 
+% tell status of supervisor and frequency servers
+w() ->
+  io:format("supervisor ~p, frequency ~p~n",[whereis(supervisor),whereis(frequency)]).
+
+% start supervisor
+start_sup() ->
+  register(supervisor, spawn(fun()->supervisor() end)).
+
+% stop supervisor
+stop_sup() ->
+  kill_sup().
+
+% stop supervisor and frequency servers
+kill_all() ->
+  kill_freq(),
+  kill_sup().
+
+% kill supervisor
+kill_sup() ->
+  case whereis(supervisor) of
+    undefined -> ok;
+    PidS -> exit(PidS, kill)
+  end.
+
+% kill frequency server
+kill_freq() ->
+  case whereis(frequency) of
+    undefined -> ok;
+    PidF -> exit(PidF, kill)
+  end.
+
+% the supervisor
+supervisor() ->
+  process_flag(trap_exit, true),    %%% Set trap
+  register(frequency, spawn_link(fun()->init() end)),
+  io:format("~p supervisor: start frequency server ~p~n",[self(),whereis(frequency)]),
+  sup_loop().
+
+% the supervisor loop
+sup_loop() ->
+  io:format("~p supervisor waiting...~n",[self()]),
+  receive
+    {'EXIT',Pid,Reason} ->
+      io:format("~p supervisor: frequency server died with reason ~p, restarting ~p~n",[self(),Reason,Pid]),
+      register(frequency, spawn_link(fun()->init() end)),
+      sup_loop();
+    Msg ->
+      io:format("~p supervisor: unexpected ~p~n",[self(),Msg]),
+      sup_loop()
+  end.
+
+% start freqency server
 start() ->
     register(frequency,
 	     spawn(frequency, init, [])).
 
+% start freqency looper
 init() ->
-  process_flag(trap_exit, true),    %%% ADDED
   Frequencies = {get_frequencies(), []},
   loop(Frequencies).
 
@@ -29,41 +96,46 @@ get_frequencies() -> [10,11,12,13,14,15].
 
 loop(Frequencies) ->
   receive
-    {request, Pid, allocate} ->
+    {request, Pid, Tag, dump} ->
+      Pid ! {reply, Tag, {ok, Frequencies}},
+      loop(Frequencies);
+    {request, Pid, Tag, allocate} ->
       {NewFrequencies, Reply} = allocate(Frequencies, Pid),
-      Pid ! {reply, Reply},
+      Pid ! {reply, Tag, Reply},
       loop(NewFrequencies);
-    {request, Pid , {deallocate, Freq}} ->
+    {request, Pid, Tag, {deallocate, Freq}} ->
       NewFrequencies = deallocate(Frequencies, Freq),
-      Pid ! {reply, ok},
+      Pid ! {reply, Tag, ok},
       loop(NewFrequencies);
-    {request, Pid, stop} ->
-      Pid ! {reply, stopped};
+    {request, Pid, Tag, stop} ->
+      Pid ! {reply, Tag, stopped};
     {'EXIT', Pid, _Reason} ->                   %%% CLAUSE ADDED
-      NewFrequencies = exited(Frequencies, Pid), 
+      NewFrequencies = exited(Frequencies, Pid),
       loop(NewFrequencies)
   end.
 
 %% Functional interface
 
-allocate() -> 
-    frequency ! {request, self(), allocate},
-    receive 
-	    {reply, Reply} -> Reply
-    end.
+dump() ->
+  {ok,Frequencies}=frequency(dump),
+  io:format("~p~n",[Frequencies]).
 
-deallocate(Freq) -> 
-    frequency ! {request, self(), {deallocate, Freq}},
-    receive 
-	    {reply, Reply} -> Reply
-    end.
+allocate() ->
+  frequency(allocate).
 
-stop() -> 
-    frequency ! {request, self(), stop},
-    receive 
-	    {reply, Reply} -> Reply
-    end.
+deallocate(Freq) ->
+  frequency({deallocate, Freq}).
 
+stop() ->
+  frequency(stop).
+
+% rpc helper
+frequency(Msg) ->
+  Tag = make_ref(),
+  frequency ! {request, self(), Tag, Msg},
+  receive
+    {reply, Tag, Reply} -> Reply
+  end.
 
 %% The Internal Help Functions used to allocate and
 %% deallocate frequencies.
@@ -81,11 +153,12 @@ deallocate({Free, Allocated}, Freq) ->
   {[Freq|Free],  NewAllocated}.
 
 exited({Free, Allocated}, Pid) ->                %%% FUNCTION ADDED
+  io:format("~p exited free=~p, allocated=~p}~n",[Pid,Free,Allocated]),
     case lists:keysearch(Pid,2,Allocated) of
       {value,{Freq,Pid}} ->
         NewAllocated = lists:keydelete(Freq,1,Allocated),
-        {[Freq|Free],NewAllocated}; 
+        {[Freq|Free],NewAllocated};
       false ->
-        {Free,Allocated} 
+        {Free,Allocated}
     end.
 
